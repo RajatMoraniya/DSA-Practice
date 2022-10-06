47d7c050705d6e8dcc5e0291e1d9e552ebe7eb8a
RajatMoraniya <rajatindian123@gmail.com>
2022-10-06 15:59:02 +0530

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/ApnaCollege/Newton/BinaryTreePep/Main$Node.class
    #	out/production/ApnaCollege/Newton/BinaryTreePep/Main$Pair.class
    #	out/production/ApnaCollege/Newton/BinaryTreePep/Main.class

--- PepCode/Graph/Graph.java
+++ PepCode/Graph/Graph.java
@@ -13,9 +13,40 @@ public class Graph {
             this.nbr = nbr;
             this.wt = wt;
         }
+        public static boolean hasPath(ArrayList<Edge>[] graph , int src , int dest , boolean[] visited){
+            if(src==dest){
+                return true;
+            }
+
+            visited[src] = true;
+            for(Edge edge : graph[src]){
+                if(visited[edge.nbr]==false){
+                    boolean hasNbrPath = hasPath(graph , edge.nbr , dest ,visited);
+                    if(hasNbrPath == true){
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
 
 
         public static void main(String[] args) throws IOException {
+
+            /*
+7
+8
+0 1 10
+1 2 10
+2 3 10
+0 3 10
+3 4 10
+4 5 10
+5 6 10
+4 6 10
+0
+6
+            */
             BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
             int vtces = Integer.parseInt(br.readLine());
@@ -36,6 +67,11 @@ public class Graph {
 
             int src = Integer.parseInt(br.readLine());
             int dest = Integer.parseInt(br.readLine());
+
+            boolean[] visited = new boolean[vtces];
+            // Has Path
+            boolean path = hasPath(graph,src,dest,visited);
+            System.out.println(path);
         }
     }
 }
